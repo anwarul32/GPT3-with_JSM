752ec34fc102a5ce448d37ccf9531be5a1e63b54
anwarul <anwarulhoque032@gmail.com>
2023-01-30 22:21:56 +0600

    added blogs section

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import './Article.css';
+import React from "react";
+import "./Article.css";
 
-const Article = () => {
-    return (
+const Article = ({ imgUrl, date, text }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog_image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
         <div>
-            <h2>Article</h2>
-        import './Article.css';</div>
-
-    );
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
-export default Article;<h2>Article</h2>
+export default Article;
+<h2>Article</h2>;
